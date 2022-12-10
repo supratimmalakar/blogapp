085897b471482f74fea1ca9b343a0baddec2f1d3
Supratim Malakar <supratimmalakar5@gmail.com>
2022-12-10 21:06:12 +0530

    Add search route

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -15,13 +15,64 @@ router.get('/allusers', verify, async (req, res) => {
 })
 
 router.get('/:userId', verify, async (req, res) => {
-    const {userId} = req.params
+    const { userId } = req.params
     try {
         const user = await User.findById(userId).exec();
         res.status(200).send(user)
     }
     catch (err) {
         res.status(500).json(err)
+    }
+})
+
+router.post('/follow', verify, async (req, res) => {
+    const { follower, followed } = req.body;
+    try {
+        const followedUser = await User.findById(followed).exec();
+        const followerUser = await User.findById(follower).exec();
+        followedUser.followers.push(follower);
+        followerUser.following.push(followed);
+        await followedUser.save();
+        await followerUser.save();
+        res.status(200).send("User followed")
+    }
+    catch (err) {
+        res.status(500).json({ err })
+    }
+})
+
+router.post('/unfollow', verify, async (req, res) => {
+    const { unfollower, unfollowed } = req.body;
+    try {
+        const unfollowedUser = await User.findById(unfollowed).exec();
+        const unfollowerUser = await User.findById(unfollower).exec();
+        const unfollowerIdx = unfollowedUser.followers.indexOf(unfollower);
+        const unfollowedIdx = unfollowerUser.following.indexOf(unfollowed);
+        unfollowedUser.followers.splice(unfollowerIdx, 1);
+        unfollowerUser.following.splice(unfollowedIdx, 1);
+        await unfollowedUser.save();
+        await unfollowerUser.save();
+        res.status(200).send("User unfollowed")
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({ err })
+    }
+})
+
+router.get('/search', verify, async (req, res) => {
+    const {q} =  req.query
+    try {
+        const users = await User.find({
+            "email" : {
+                '$regex' : q,
+                '$options' : "i"
+            }
+        }).exec();
+        res.status(200).json(users);
+    }
+    catch (err) {
+        res.status(400).json(err)
     }
 })
 
