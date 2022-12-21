ef6ca973a7c816f2584cbf08823b61f4581d3f6d
Supratim Malakar <supratimmalakar5@gmail.com>
2022-12-22 00:03:29 +0530

    Add update-bio feature & show followers/following list

--- client/pages/dashboard/profile.js
+++ client/pages/dashboard/profile.js
@@ -1,16 +1,77 @@
 import axios from 'axios';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Layout from '../../components/Layout'
 import { useFetch } from '../../utils/useFetch'
 import PostBox from '../../components/PostBox'
+import List from '../../components/List';
+import { openToast, errorToast } from '../../redux/toastReducer';
+import { useDispatch } from 'react-redux';
 
 
 function Profile({ user, token, posts }) {
-    
-
+    const dispatch = useDispatch();
+    const followerRef = useRef();
+    const followingRef = useRef();
+    const [edit, setEdit] = useState(false);
+    const [bio, setBio] = useState(false)
+    const [list1Offset, setList1Offset] = useState({
+        top: null,
+        left: null
+    })
+    const [list2Offset, setList2Offset] = useState({
+        top: null,
+        left: null
+    })
+    const [open1, setOpen1] = useState(false)
+    const [open2, setOpen2] = useState(false)
     const { data, error, loaded } = useFetch(`/user/${user.id}`, "get", token);
-    console.log({id: user.id})
+    useEffect(() => {
+        if (loaded && !error) {
+
+            setList1Offset({
+                top: followerRef.current.offsetTop,
+                left: followerRef.current.offsetLeft
+            })
+            setList2Offset({
+                top: followingRef.current.offsetTop,
+                left: followingRef.current.offsetLeft
+            })
+            setBio(data.bio)
+        }
+
+    }, [loaded])
+
+    const updateBioHandler = async () => {
+        if (bio.length <= 100) {
+            try {
+                await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/update-bio`, {
+                    userId: user.id,
+                    bio
+                }, {
+                    headers: {
+                        Authorization: 'Bearer ' + token
+                    }
+                })
+                setEdit(false)
+                dispatch(openToast({
+                    message : "Bio updated!",
+                    severity : "success"
+                }))
+                location.reload()
+            }
+            catch (err) {
+                console.log(err)
+                dispatch(errorToast())
+            }
+        }
+        else {
+            dispatch(openToast({
+                message: "Bio must be shorter than 100 characters",
+                severity: "error"
+            }))
+        }
+    }
 
 
     return (
@@ -18,16 +79,29 @@ function Profile({ user, token, posts }) {
             <div className='flex flex-row justify-start gap-[50px]'>
                 {loaded && !error &&
                     (<>
+                        <List title="Followers" open={open1} setOpen={setOpen1} token={token} listOffset={list1Offset} items={data.followers} />
+                        <List title="Following" open={open2} setOpen={setOpen2} token={token} listOffset={list2Offset} items={data.following} />
                         <div className='bg-[rgba(0,0,0,0.4)] w-[120px] h-[120px] rounded-full flex justify-center items-center'>
                             <h1 className='text-white text-[48px]'>{data.fname[0] + data.lname[0]}</h1>
                         </div>
                         <div className='flex flex-col max-w-[350px]'>
                             <div className='flex flex-row gap-[15px]'>
-                                <p><span className='font-bold'>{data.followers.length}</span> followers</p>
-                                <p><span className='font-bold'>{data.following.length}</span> following</p>
+                                <p className='cursor-pointer' onClick={() => setOpen1(true)} ref={followerRef}><span className='font-bold'>{data.followers.length}</span> followers</p>
+                                <p className='cursor-pointer' onClick={() => setOpen2(true)} ref={followingRef}><span className='font-bold'>{data.following.length}</span> following</p>
                             </div>
                             <h3 className='font-[500]'>{data.fname + ' ' + data.lname} </h3>
-                            <h4 className='font-[400]'>{data.bio}</h4>
+                            {edit ?
+                                <div className='flex flex-col items-start gap-[20px] margin'>
+                                    <textarea className='w-[300px] h-[80px] border-2 rounded p-2' value={bio} onChange={(e) => setBio(e.target.value)} />
+                                    <button onClick={updateBioHandler}>Save</button>
+                                </div>
+                                :
+                                <div className='flex flex-col items-start gap-[20px] margin'>
+                                    <h4 className='font-[400]'>{data.bio}</h4>
+                                    <button onClick={() => setEdit(true)}>{data.bio ? "Edit bio" : "Add bio"}</button>
+                                </div>
+                            }
+
                         </div>
                     </>)}
             </div>
--- client/pages/dashboard/users/[userId]/index.js
+++ client/pages/dashboard/users/[userId]/index.js
@@ -1,16 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Layout from '../../../../components/Layout'
 import { useRouter } from 'next/router'
 import { useFetch } from '../../../../utils/useFetch'
 import axios from 'axios'
 import PostBox from '../../../../components/PostBox'
+import List from '../../../../components/List'
+import { openToast, errorToast } from '../../../redux/toastReducer';
+import { useDispatch } from 'react-redux';
 
 function UserProfile({ user, token }) {
+    const dispatch = useDispatch()
     const router = useRouter();
     const [userFollowers, setUserFollowers] = useState([])
     const { userId } = router.query;
-    const { data, error, loaded } = useFetch(`/user/${userId}`, "get", token);
+    const followerRef = useRef();
+    const followingRef = useRef();
+    const [list1Offset, setList1Offset] = useState({
+        top: null,
+        left: null
+    })
+    const [list2Offset, setList2Offset] = useState({
+        top: null,
+        left: null
+    })
+    const [open1, setOpen1] = useState(false)
+    const [open2, setOpen2] = useState(false)
     const { data: posts, error: postsError, loaded: postsLoaded } = useFetch(`/posts/${userId}`, "get", token);
+    const { data, error, loaded } = useFetch(`/user/${userId}`, "get", token);
+    useEffect(() => {
+        if (loaded) {
+
+            setList1Offset({
+                top: followerRef.current.offsetTop,
+                left: followerRef.current.offsetLeft
+            })
+            setList2Offset({
+                top: followingRef.current.offsetTop,
+                left: followingRef.current.offsetLeft
+            })
+        }
+
+    }, [loaded])
 
     useEffect(() => {
         if (user.id === userId)
@@ -41,7 +71,7 @@ function UserProfile({ user, token }) {
                     .then(res => setUserFollowers([...res.data.followers]))
                     .catch(err => console.log(err))
             })
-            .catch(err => console.log(err))
+            .catch(err => dispatch(errorToast()))
     }
 
     const followUser = async () => {
@@ -62,20 +92,22 @@ function UserProfile({ user, token }) {
                     .then(res => setUserFollowers([...res.data.followers]))
                     .catch(err => console.log(err))
             })
-            .catch(err => console.log(err))
+            .catch(err => dispatch(errorToast()))
     }
     return (
         <Layout token={token} user={user} className='p-[20px]'>
             <div className='flex flex-row justify-start gap-[50px]'>
                 {loaded && !error &&
                     (<>
+                        <List title="Followers" open={open1} setOpen={setOpen1} token={token} listOffset={list1Offset} items={data.followers} />
+                        <List title="Following" open={open2} setOpen={setOpen2} token={token} listOffset={list2Offset} items={data.following} />
                         <div className='bg-[rgba(0,0,0,0.4)] w-[120px] h-[120px] rounded-full flex justify-center items-center'>
                             <h1 className='text-white text-[48px]'>{data.fname[0] + data.lname[0]}</h1>
                         </div>
                         <div className='flex flex-col max-w-[350px]'>
                             <div className='flex flex-row gap-[15px]'>
-                                <p><span className='font-bold'>{userFollowers.length}</span> followers</p>
-                                <p><span className='font-bold'>{data.following.length}</span> following</p>
+                                <p className='cursor-pointer' onClick={() => setOpen1(true)} ref={followerRef}><span className='font-bold'>{userFollowers.length}</span> followers</p>
+                                <p className='cursor-pointer' onClick={() => setOpen2(true)} ref={followingRef}><span className='font-bold'>{data.following.length}</span> following</p>
                             </div>
                             <h3 className='font-[500]'>{data.fname + ' ' + data.lname} </h3>
                             <h4 className='font-[400]'>{data.bio}</h4>
