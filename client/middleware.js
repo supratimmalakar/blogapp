bd054931779fb4c84aae80746f16d1544d41c783
Supratim Malakar <supratimmalakar5@gmail.com>
2022-12-25 23:29:36 +0530

    Update redirection routes

--- client/middleware.js
+++ client/middleware.js
@@ -4,11 +4,11 @@ export default function middleware(req, res, next) {
     const { cookies, url } = req;
     const token = cookies.get('blogToken')?.value
     if (token && url.includes('login')) {
-        if (token) return NextResponse.redirect('http://localhost:3000/dashboard')
+        if (token) return NextResponse.redirect('https://blogapp-supratimmalakar.vercel.app/dashboard')
         else return NextResponse.next()
     }
     if (url.includes('dashboard')) {
-        if (!token) return NextResponse.redirect('http://localhost:3000/login')
+        if (!token) return NextResponse.redirect('https://blogapp-supratimmalakar.vercel.app/login')
         else return NextResponse.next();
     }
 }
